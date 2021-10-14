f8ed7421fb6d8f018f3eb3c474d52291d2086e22
andrea1228 <92496067+andrea1228@users.noreply.github.com>
2021-10-14 12:20:50 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,75 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . d . . d . . 1 . . . . . . . 
+. . d d . d . 1 1 . . . . . . . 
+. d d d . d d 1 . . . . . . . . 
+. d d d . d 1 1 d . d . . . . . 
+. d d d . d 1 d . 1 d . . . . . 
+. d d d 1 d 1 d 1 d d . . . . . 
+1 d d d 1 d 1 d 1 d d 1 . . . . 
+. d d d d 1 1 d d d d 1 . . . . 
+. d 1 d d 1 d 1 d d d 1 . . . . 
+. d . d d 1 d d 1 d d 1 . . . . 
+. d . 1 d . d d 1 d d 1 . . . . 
+. . . 1 . . d d 1 d d . . . . . 
+. . . . . . 1 1 d 1 1 . . . . . 
+. . . . . . . . 1 d 1 . . . . . 
+. . . . . . . . 1 1 1 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. d d d d . . 7 7 7 7 . . . . . 
+d d d d d . d 7 7 . . 7 d d . . 
+d d d d d d d d d d d d 7 d d . 
+d d b b d . d . 7 d d . d d d . 
+d . d d d d d b d d d d 7 d d . 
+. d . b d d b d d d d d d d d . 
+d d . d d b d d d d d 7 d d d . 
+. . 7 d d d d d d d 7 d b d d d 
+7 7 7 d d d d d d d d 7 d d d d 
+7 7 d d d d d d d d . d d d . . 
+7 . d d d d d d d 7 d d d . . . 
+7 d d d d d d 7 7 d d d 7 7 d d 
+d . d d d d 7 . d d 7 7 d d d . 
+. d 7 d d 7 7 d d . 7 d d d . . 
+. . 7 7 7 7 d d 7 . d d 7 7 d d 
+. . . 7 7 d d . . 7 d . . d . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
